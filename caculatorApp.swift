ce5a904535baf439673ae22c90155d53af65367f
HA5H1M16 <155622731+HA5H1M16@users.noreply.github.com>
2024-05-06 10:31:57 +0200

    Initial commit

---
+++ caculatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  caculatorApp.swift
+//  caculator
+//
+//  Created by Mohammad Hasham Ahmed on 14/03/2024.
+//
+
+import SwiftUI
+
+@main
+struct caculatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
